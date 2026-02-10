d62a2dc4b39d906b6feb9628309f94b55762af05
Anna1983-lang <rigikanuta@gmail.com>
2026-02-10 09:25:06 +0200

    1

---
+++ models/news.py
@@ -0,0 +1,29 @@
+# article.py
+# Модель статьи
+# Проект: "Автомобильный завод"
+
+from app import db
+
+
+class Article(db.Model):
+    __tablename__ = "articles"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    title = db.Column(
+        db.String(200),
+        nullable=False
+    )
+
+    content = db.Column(
+        db.Text,
+        nullable=False
+    )
+
+    category = db.Column(
+        db.String(50),
+        nullable=False
+    )
+
+    def __repr__(self):
+        return f"<Article {self.title}, category={self.category}>"
