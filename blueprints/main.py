580f9a7653f5b4a327b09209269cadd6185135e2
Anna1983-lang <rigikanuta@gmail.com>
2026-02-10 09:20:50 +0200

    Create main.py

---
+++ blueprints/main.py
@@ -0,0 +1,9 @@
+# main.py
+# Точка входа в веб-приложение
+# Корпоративный сайт "Автомобильный завод"
+
+from app import app
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
